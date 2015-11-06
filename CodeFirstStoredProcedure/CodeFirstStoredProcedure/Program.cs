10e93e85b08bcf8f899c1f3f2f657d91d644a158
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-11-06 10:03:37 +0530

    Added code to use stored procedure with entity framework code first.

--- CodeFirstStoredProcedure/CodeFirstStoredProcedure/Program.cs
+++ CodeFirstStoredProcedure/CodeFirstStoredProcedure/Program.cs
@@ -1,10 +1,28 @@
-﻿
+﻿using System;
+using System.Linq;
+
 namespace CodeFirstStoredProcedure
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (EmployeeDbContext dbContext = new EmployeeDbContext())
+            {
+                string commandText = "[dbo].[usp_GetAllEmployees]";
+                var employees = dbContext.Database.SqlQuery<Employee>(commandText).ToList();
+
+                Console.WriteLine("Printing Employee");
+                foreach (var employee in employees)
+                {
+                    Console.WriteLine(employee.EmployeeId);
+                    Console.WriteLine(employee.FirstName);
+                    Console.WriteLine(employee.LastName);
+                    Console.WriteLine(employee.Designation);
+                    Console.WriteLine("-----------------");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
